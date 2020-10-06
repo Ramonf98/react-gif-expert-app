1336a6d29f9b655ea2e4d84a83565125aecf7a58
Ramon Cordova <is.ramoncordova@gmail.com>
2020-10-06 13:28:36 -0700

    docs agregado

---
+++ docs/precache-manifest.80e8488c1d0a5c899c865543a884768a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99e1d2e142b903f7dcda9f2a6c9f2f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405779016d8081ab5a6a",
+    "url": "/static/css/main.f4cf0a00.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405779016d8081ab5a6a",
+    "url": "/static/js/main.3ded236f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  },
+  {
+    "revision": "a2166f0cf0f537b9d07c2d2ffc88bacb",
+    "url": "/static/media/Fondo1.a2166f0c.png"
+  }
+]);
